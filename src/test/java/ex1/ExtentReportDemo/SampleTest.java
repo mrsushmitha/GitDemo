432d6b0a41918e711a9da254752f64f109103699
mrsushmitha <mr.sushmitha@gmail.com>
2020-09-25 17:56:29 -0400

    This is from usa guy

--- src/test/java/ex1/ExtentReportDemo/SampleTest.java
+++ src/test/java/ex1/ExtentReportDemo/SampleTest.java
@@ -5,6 +5,8 @@ public class SampleTest {
 	public static void main(String[] args) {
 		System.out.println("TEST 1");
 		System.out.println("TEST 2");
+		System.out.println("US-TEST 3");
+		System.out.println("US-TEST 4");
 
 	}
 
